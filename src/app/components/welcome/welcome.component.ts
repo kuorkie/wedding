06dce7bf27d42c97e093f59a19473e059258075c
yeldarl2808 <6cdh5j-TR9znY0>
2025-02-19 17:25:10 +0500

    fixed

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -14,12 +14,7 @@ export class WelcomeComponent {
 
   }
   ngOnInit() {
-    let  i = document.getElementById('text')
-    setTimeout(()=>{
-      if(i){
-        i.style.opacity = "0.85"
-      }
-    },2600)
+
   }
 
 
